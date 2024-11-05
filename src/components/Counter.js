cd2a479bf964de9f24bd51e26d3489f7426e6e2d
Perpetual Akinyi <you@example.com>
2024-11-05 10:41:13 +0300

    used state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,11 +3,14 @@ import React, { useState } from "react";
 function Counter() {
   const [count, setCount] = useState(0);
 
-  function increment() {
-    setCount(count + 1);
+  function increment(){
+    
+    setCount(count + 1)
+
   }
+  return(
+    <button  onClick={increment}  > Hey count me :{count}</button>
+  )
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
 }
-
-export default Counter;
+export  default Counter; 
